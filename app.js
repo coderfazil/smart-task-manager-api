01745e3135eb4e0cadc9fbf960e11925e75263e0
mohammad-fazil001 <mohammd.fazil@leadtorev.com>
2026-02-16 17:35:13 +0530

    added some security middlewares in the app

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ const cookieParser = require('cookie-parser');
 const {errorMiddleware} = require("./middlewares/errorMiddleware");
 const authRouter= require("./routes/authRoutes");
 const taskRouter = require("./routes/taskRoutes");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
+
+app.set("trust proxy", 1);
+
+app.use(helmet());
+
+function mongoSanitize() {
+    return undefined;
+}
+
+app.use(mongoSanitize());
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +36,10 @@ app.use(cors({
 }));
 app.use("/auth",authRouter);
 app.use(taskRouter);
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
 
 app.use(errorMiddleware);
 module.exports = app;
